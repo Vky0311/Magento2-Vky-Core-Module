e1bf7078054bbf895824348de2965452ebaba5b3
Vicky Darji <35851452+Vky0311@users.noreply.github.com>
2018-05-05 22:45:40 +0530

    first commit
    first commit

---
+++ Vky/Core/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category   Vky
+ * @package    Vky_Core
+ * @author     Vicky <viky.031290@gmail.com>
+ * @copyright  Copyright © VKY All right reserved
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vky_Core',
+    __DIR__
+);
